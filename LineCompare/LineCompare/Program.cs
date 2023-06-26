d5ff41f346914e9c875ce27f526b51e54b41c0e9
“codewitherrors” <codewitherrors1@gmail.com>
2023-06-26 09:43:17 +0530

    Add : Find The Length of Line

---
+++ LineCompare/LineCompare/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineCompare
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            LineCom f1 = new LineCom(); 
+            f1.fanOfGeometry();
+        } 
+    }
+}
